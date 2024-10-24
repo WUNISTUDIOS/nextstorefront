b5d356c18bf0fcb4f0352bbeb1731c138369ad54
WUNISTUDIOS <wunistudios@gmail.com>
2024-10-24 11:37:44 -0400

    confirmation token

---
+++ components/auth/email-verification-form.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/navigation"
+import { useCallback, useEffect, useState } from "react"
+import { AuthCard } from "./auth-card"
+import { FormSuccess } from "./form-success"
+import { FormError } from "./form-error"
+import { newVerification } from "@/server/actions/tokens"
+
+export const EmailVerificationForm = () => {
+    const token = useSearchParams().get("token")
+    const router = useRouter()
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
+
+    const handleVerificaton = useCallback(()=>{
+        if(success || error) return
+        if(!token){ setError("Invalid token")
+            return 
+        }
+        newVerification(token).then((data:{error?: string; success?: string}) => {
+            if(data.error){
+                setError(data.error)
+            }
+            if(data.success){
+                setSuccess(data.success)
+                router.push("auth/login")
+            }
+        })
+    },[])
+
+    useEffect(()=>{
+        handleVerificaton()
+    },[])
+    return (
+        <AuthCard 
+                    backButtonLabel="back to login" 
+                    backButtonHref="/auth/login" 
+                    cardTitle="verify your account"
+                >
+                <div className="flex items-center flex-col w-full justify-center">
+                    <p>{!success && !error? "verifying email.." : null}</p>
+                    <FormSuccess message={success}/>
+                    <FormError message={error}/>
+                </div>
+        </AuthCard>
+    )
+}
--- server/actions/tokens.ts
+++ server/actions/tokens.ts
@@ -1,6 +1,7 @@
 "use server"
+import { error } from "console"
 import {db} from "../db"
-import { emailTokens } from "../schema"
+import { emailTokens, users } from "../schema"
 import { eq } from "drizzle-orm"
 
 export const getVerificationTokenByEmail = async (email: string) => {
@@ -18,10 +19,23 @@ export const generateEmailVerificationToken = async (email: string) => {
     const token = crypto.randomUUID()
     const expires = new Date(new Date().getTime() + 3600 * 1000)
     const existingToken = await getVerificationTokenByEmail(email)
-
     if(existingToken){
         await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id))
     }
     const verificationToken = await db.insert(emailTokens).values({email, token, expires,}).returning()
     return verificationToken
+}
+
+export const newVerification = async (token: string) => {
+    const existingToken = await getVerificationTokenByEmail(token)
+    if(!existingToken) return {error: "Invalid token"}
+    const hasExpired = new Date(existingToken.expires) < new Date()
+    if(hasExpired) return{error: "Token has expired"}
+    const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, existingToken.email)
+    })
+    if(!existingUser) return {error: "email not found"}
+    await db.update(users).set({emailVerified: new Date(), email: existingToken.email,})
+    await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id))
+    return {success: "email verified"}
 }
